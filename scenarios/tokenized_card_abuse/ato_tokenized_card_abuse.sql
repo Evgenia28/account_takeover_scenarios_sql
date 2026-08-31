9e6f0e1eaba986b3194947adde9398ac62d56d28
Evgenia28 <145423977+Evgenia28@users.noreply.github.com>
2026-08-31 15:23:05 +0100

    Refactor SQL query for tokenized card abuse detection
    Updated SQL query to use specific dataset references for logins and deposits. Added login gap in hours and adjusted conditions for filtering deposits.

--- scenarios/tokenized_card_abuse/ato_tokenized_card_abuse.sql
+++ scenarios/tokenized_card_abuse/ato_tokenized_card_abuse.sql
@@ -3,7 +3,7 @@
 -- Output: user_id
 WITH active_logins AS (
   SELECT DISTINCT user_id
-  FROM logins 
+  FROM code-testing-ato.tokenized_ato_dep_test.tca_logins
   WHERE login_datetime > DATETIME_SUB(CURRENT_TIMESTAMP(), INTERVAL 24 HOUR)
 ),
 -- CTE 2: Identifies digital fingerprint change and time gap between logins
@@ -17,12 +17,13 @@ login_change AS (
          LAG(l.iso2) OVER(PARTITION BY al.user_id ORDER BY l.login_datetime) AS prev_iso2,
          l.device_type AS current_device,
          LAG(l.device_type) OVER(PARTITION BY al.user_id ORDER BY l.login_datetime) AS prev_device,
-         DATETIME_DIFF(l.login_datetime, LAG(l.login_datetime) OVER(PARTITION BY al.user_id ORDER BY l.login_datetime), HOUR) AS login_gap
-  FROM logins l 
+         DATETIME_DIFF(l.login_datetime, LAG(l.login_datetime) OVER(PARTITION BY al.user_id ORDER BY l.login_datetime), HOUR) AS login_gap_hrs
+  FROM code-testing-ato.tokenized_ato_dep_test.tca_logins l 
   JOIN active_logins al ON l.user_id = al.user_id
   QUALIFY current_iso2 != prev_iso2
   AND current_device != prev_device
-  AND login_gap < 24       
+  AND last_login > DATETIME_SUB(CURRENT_TIMESTAMP(), INTERVAL 24 HOUR)
+  AND login_gap_hrs < 24       
 ),
 -- CTE 3 Aggregates financial data, detects existing card draining
 -- Input: deposits, login_change
@@ -35,10 +36,10 @@ financials AS (
          SUM(CASE WHEN d.status = 'approved' THEN d.amount END) AS actual_total_deposit,
          SUM(amount) AS total_attempted_deposit
   FROM login_change lc
-  LEFT JOIN deposits d ON lc.user_id = d.user_id
+  LEFT JOIN code-testing-ato.tokenized_ato_dep_test.tca_deposits d ON lc.user_id = d.user_id
   -- Making time filter part of the matching condition in JOIN
   -- Filtering inside the ON clause evaluates the condition during the join
-  AND d.created_at BETWEEN lc.last_login AND DATETIME_ADD(lc.last_login, INTERVAL 30 MINUTE)
+  AND d.dep_datetime BETWEEN lc.last_login AND DATETIME_ADD(lc.last_login, INTERVAL 30 MINUTE)
   GROUP BY lc.user_id
 )
 -- Final Query displays an overview of login and financials data
@@ -47,7 +48,7 @@ SELECT
        f.user_id,
        lc.current_iso2,
        lc.prev_iso2,
-       lc.login_gap,
+       lc.login_gap_hrs,
        lc.current_device,
        lc.prev_device,
        COALESCE(f.approved_dep_count, 0) AS approved_dep_count,
@@ -56,11 +57,11 @@ SELECT
        COALESCE(f.total_attempted_deposit, 0) AS total_attempted_deposit
 FROM login_change lc
 JOIN financials f ON lc.user_id = f.user_id
-  
+;
 Sample Output:
   
-| user_id | current_iso2 | prev_iso2 | login_gap | current_device | prev_device | approved_dep_count | failed_dep_count | actual_total_deposit | attempted_total_deposit |
-|---------|--------------|-----------|-----------|----------------|-------------|--------------------|------------------|----------------------|-------------------------|
-| 10042   | GB           | DE        | 3         | mobile         | desktop     | 4                  | 2                | 850.00               | 1150.00                 |
-| 10187   | GB           | DE        | 7         | mobile         | desktop     | 6                  | 0                | 2400.00              | 2400.00                 |
-| 10391   | GB           | DE        | 1         | mobile         | desktop     | 2                  | 3                | 320.00               | 770.00                  |
+| Row | user_id | current_iso2 | prev_iso2 | login_gap_hrs | current_device | prev_device | approved_dep_count | failed_dep_count | actual_total_deposit | total_attempted_deposit |
+|-----|---------|--------------|-----------|---------------|----------------|-------------|--------------------|------------------|----------------------|-------------------------|
+| 1   | 30001   | AU           | GB        | 16            | mobile         | desktop     | 3                  | 1                | 1600.0               | 2800.0                  |
+| 2   | 30002   | DE           | GB        | 7             | mobile         | desktop     | 1                  | 0                | 150.0                | 150.0                   |
+| 3   | 30003   | US           | GB        | 4             | mobile         | desktop     | 0                  | 0                | 0.0                  | 0.0                     |
